74d3f2e8f0f627ede93697bc6109e9776a60a2e2
Dale Hudson <hudsonn90@gmail.com>
2017-02-22 21:46:53 +0000

    [Bootstrap] Autoloader Added model location class

--- bootstrap.php
+++ bootstrap.php
@@ -4,4 +4,5 @@
 
 \Autoloader::add_classes(array(
 	'FuelPostcodeIO\\PostcodeIO' => __DIR__ . '/classes/postcodeio.php',
+	'FuelPostcodeIO\\Model_Location' => __DIR__ . '/classes/model/location.php',
 ));
